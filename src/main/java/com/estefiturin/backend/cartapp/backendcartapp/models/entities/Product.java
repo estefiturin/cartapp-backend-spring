f82d9bd1d2470166e8be63444ad8999a93dd518c
Estefanía Turín <tur.senn@gmail.com>
2023-07-06 20:58:46 -0300

    POJO class product

---
+++ src/main/java/com/estefiturin/backend/cartapp/backendcartapp/models/entities/Product.java
@@ -0,0 +1,59 @@
+package com.estefiturin.backend.cartapp.backendcartapp.models.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//POJO
+@Entity
+@Table(name = "products") // nombre base de datos en SQL
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // crea el id de forma incremental  
+    private Long id;
+    
+    private String name;
+
+    private String description; 
+
+    private Long price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    
+    
+
+    
+}
