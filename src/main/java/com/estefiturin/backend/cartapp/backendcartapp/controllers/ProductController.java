df43cf55b915e03328fec7db9c133db8f06ac7be
Estefanía Turín <tur.senn@gmail.com>
2023-07-07 17:06:11 -0300

    comunicacion con cart-app

--- src/main/java/com/estefiturin/backend/cartapp/backendcartapp/controllers/ProductController.java
+++ src/main/java/com/estefiturin/backend/cartapp/backendcartapp/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.estefiturin.backend.cartapp.backendcartapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.estefiturin.backend.cartapp.backendcartapp.models.entities.Product;
 import com.estefiturin.backend.cartapp.backendcartapp.services.ProductService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173/")
 public class ProductController {
 
     @Autowired
